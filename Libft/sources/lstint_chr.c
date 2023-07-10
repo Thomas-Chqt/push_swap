cc76ac0e708b1fa603c16b5d0bce3ad5c3bd1be0
Thomas <tchoquet@student.42tokyo.jp>
2023-07-10 20:37:19 +0900

    fix norm

--- Libft/sources/lstint_chr.c
+++ Libft/sources/lstint_chr.c
@@ -6,11 +6,13 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 17:10:37 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/07/09 17:15:24 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/07/10 20:33:07 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_internal.h"
+
+typedef t_bool	(*t_func)(int, void*);
 
 static t_bool	test_func_wrap(void *nbr, void *data);
 
@@ -26,7 +28,7 @@ t_list	*lstint_chr(t_list *lst, t_bool (*test)(int, void *), void *data)
 static t_bool	test_func_wrap(void *nbr, void *data)
 {
 	int		inbr;
-	t_bool	(*test_func)(int, void *);
+	t_func	test_func;
 
 	inbr = *((int *)nbr);
 	test_func = ((void **)data)[0];
